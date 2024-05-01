756c95a9ed4bb728778fc7a9279c91f7e1b8bdae
Samuel Bretiere <samuel.bretiere@etu.esisar.grenoble-inp.fr>
2024-05-01 12:47:13 +0200

    Fix fill hprod

--- controller.py
+++ controller.py
@@ -264,7 +264,7 @@ def reload_DB():
 # Recuring Tasks
 async def recurring_task():
     await bot.wait_until_ready()
-    fillDB.fill()
+    if os.environ.get("Prod") == "true" : fillDB.fill()
     while not bot.is_closed():
         schedule.run_pending()
         await asyncio.sleep(1)
