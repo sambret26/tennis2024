6b26676a86b3459927cf0e5fd1114d2b01868350
Samuel Bretiere <samuel.bretiere@etu.esisar.grenoble-inp.fr>
2024-05-05 14:33:54 +0200

    Update calendar token

--- functions.py
+++ functions.py
@@ -4,6 +4,7 @@
 from discord_slash.utils.manage_components import create_button, create_actionrow, wait_for_component
 from google_auth_oauthlib.flow import InstalledAppFlow as IAF
 from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 from discord_slash import ButtonStyle
 from datetime import datetime as date
 from datetime import timedelta
@@ -11,7 +12,7 @@ import os.path as path
 import subprocess
 import discord
 import pickle
-import update
+import json
 import os
 
 # My packages
@@ -23,6 +24,7 @@ from databases.repositories.courtsRepository import CourtsRepository
 from databases.repositories.teamsRepository import TeamsRepository
 from databases.base import engine, session
 from constants import constants
+import update
 
 messagesRepository = MessagesRepository(engine)
 channelsRepository = ChannelsRepository(engine)
@@ -317,19 +319,53 @@ def load(name):
     with open("DB/" + name + ".pkl", 'rb') as f:
         return pickle.load(f)
 
+def load_crypted(name):
+    with open('DB/' + name + '.pkl', 'rb') as f:
+        crypted = pickle.load(f)
+        return decode_credentials(crypted)
+
+def decode_credentials(encrypted_credentials):
+    password = os.environ.get('HashPassword')
+
+    decrypted_data = ""
+    for i in range(len(encrypted_credentials)):
+        d = encrypted_credentials[i]
+        p = password[i % len(password)]
+        v = chr(ord(d) - ord(p))
+        decrypted_data += v
+
+    creds_dict = json.loads(decrypted_data)
+    creds = Credentials.from_authorized_user_info(creds_dict)
+    obj = {"credentials" : creds, "valid" : creds_dict["valid"], "expired" : creds_dict["expired"]}
+    return obj
+
+def code_credentials(credentials):
+    password = os.environ.get('HashPassword')
+    serialized_credentials = credentials.to_json()
+    credentials_dict = json.loads(serialized_credentials)
+    credentials_dict["valid"] = credentials.valid
+    credentials_dict["expired"] = credentials.expired
+    updated_serialized_credentials = json.dumps(credentials_dict)
+    encrypted_data = ""
+    for i in range(len(updated_serialized_credentials)):
+        d = updated_serialized_credentials[i]
+        p = password[i % len(password)]
+        v = chr(ord(d) + ord(p))
+        encrypted_data += v
+
+    return encrypted_data
+
 def findCreds():
     creds = None
     filename = os.environ.get("TokenFilename")
     if path.exists("DB/" + filename + ".pkl"):
-        creds = load(filename)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = IAF.from_client_secrets_file(os.environ.get("CredentialsFilename"), os.environ.get("Scopes"))
-            creds = flow.run_local_server(port=0)
+        obj = load_crypted(filename)
+        creds = obj["credentials"]
+    if not creds or not obj["valid"]:
+        creds.refresh(Request())
+        crypted_creds = code_credentials(creds)
         with open("DB/" + filename + ".pkl", 'wb') as token:
-            pickle.dump(creds, token)
+            pickle.dump(crypted_creds, token)
     return creds
 
 async def importDB(message):
